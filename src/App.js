985b1c4798782ba3efa8e4505d87e573960aca82
Luis Enrique <luyo.lemp@gmail.com>
2020-03-30 23:05:40 -0300

    create folders for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import 
+
+const Home = () => (
+  <>
+    <h1 style={testStyle}>Hola Mundo</h1>
+  </>
+)
+
+export default Home
+
+const testStyle = {
+  color: '#f00'
+}
